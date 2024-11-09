47730b062410983924ddaa96ffd8e709da561322
stan <s.nikolov1000@gmail.com>
2024-11-09 15:29:53 +0000

    changed to relative file paths and removed redundant camera logic

--- mainwindow.h
+++ mainwindow.h
@@ -30,8 +30,8 @@ public:
     QVector<CameraCell *> allCellPtr;
     QList<QCameraDevice> availableCameras = QMediaDevices::videoInputs();
     QCameraDevice defaultCamera = QMediaDevices::defaultVideoInput();
-    std::string rtm_detnano_onnx_path = "/home/stanisaur/Documents/qtprojects/3DPoseInput/PoseEstimation/models/rtmdet-389d3a.onnx";
-    std::string rtm_pose_onnx_path = "/home/stanisaur/Documents/qtprojects/3DPoseInput/PoseEstimation/models/rtmpose-s_simcc-body7_pt-body7_420e.onnx";
+    std::string rtm_detnano_onnx_path = "../../PoseEstimation/models/rtmdet-389d3a.onnx";
+    std::string rtm_pose_onnx_path = "../../PoseEstimation/models/rtmpose-s_simcc-body7_pt-body7_420e.onnx";
     void processAndDisplayImages();
 private slots:
     void on_pushButton_clicked();
@@ -42,10 +42,6 @@ private slots:
 
 private:
     Ui::MainWindow *ui;
-    //just keeps track of whether or not camera is being used at a given time, uses device id
-    //for simplification and to ensure no room for error with Qt's sus as shiet librarys that may be unpredictable
-    //0: not being used, 1: being used
-    std::unordered_map<int, int> cameraUsage;
     std::unique_ptr<RTMPoseTrackerOnnxruntime> rtmpose_tracker_onnxruntime;
     cv::VideoCapture cap;
     QTimer *mainTimer;
